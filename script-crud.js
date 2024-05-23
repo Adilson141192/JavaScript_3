1d17fb68dedb471daad0ab9632fe51b442d65cca
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2024-05-23 12:15:03 -0300

    052324 aula0105

--- script-crud.js
+++ script-crud.js
@@ -2,7 +2,7 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
-const textaArea = document.querySelector('.app__form-textarea')
+const textArea = document.querySelector('.app__form-textarea')
 
 const tarefas = []
 
@@ -13,8 +13,8 @@ btnAdicionarTarefa.addEventListener('click', () => {
 formAdicionarTarefa.addEventListener('submit', (evento) => {
     evento.preventDefault();
     const tarefa = {
-        descricao: textaArea.value
+        descricao: textArea.value
     }
-    tarefa.push(tarefa)
-    localStorage.setItem('tarefas', tarefas)
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
